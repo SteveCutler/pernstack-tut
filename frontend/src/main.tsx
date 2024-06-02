836a71aad0f3a869ab139ce70a51b8fef1204333
SteveCutler <stephen.c.cutler@gmail.com>
2024-06-02 07:57:15 -0400

    authcontext.tsx added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { AuthContextProvdider } from './context/AuthContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <AuthContextProvdider>
+                <App />
+            </AuthContextProvdider>
         </BrowserRouter>
     </React.StrictMode>
 )
